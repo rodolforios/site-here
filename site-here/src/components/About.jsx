dfeb240a6e265c754ac15004557fcdd20ae002ba
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2022-08-25 16:57:15 -0300

    correção header

--- site-here/src/components/About.jsx
+++ site-here/src/components/About.jsx
@@ -6,14 +6,14 @@ const About = () => {
 
 
 
-            <div class="relative bg-white  p-4 mt-32 ">
-                <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
-                    <div class="lg:col-start-2 md:pl-20 mr-11">
-                        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">Suba de patamar <span class="text-rose-600 ">com a Top #1</span>  em desenvolvimento!.</h1>
-                        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Na Here trazemos ideias para a realidade, com soluções criativas, tecnologias mais recentes e ferramentas poderosas!</p>
+            <div className="relative bg-white  p-4 mt-32 ">
+                <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
+                    <div className="lg:col-start-2 md:pl-20 mr-11 ">
+                        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl px-4">Suba de patamar <span className="text-rose-600 ">com a Top #1</span>  em desenvolvimento!</h1>
+                        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Na Here trazemos ideias para a realidade, com soluções criativas, tecnologias mais recentes e ferramentas poderosas!</p>
                     </div>
-                    <div class="mt-10 -mx-4 md:-mx-12 relative lg:mt-0 lg:col-start-1 ">
-                        <img src="/about.gif" alt="illustration" class="relative mx-auto  rounded w-auto" />
+                    <div className="mt-10 -mx-4 md:-mx-12 relative lg:mt-0 lg:col-start-1 ">
+                        <img src="/about.gif" alt="illustration" className="relative mx-auto px-3  rounded w-auto" />
                     </div>
                 </div>
             </div>
--- site-here/src/components/Header.jsx
+++ site-here/src/components/Header.jsx
@@ -53,7 +53,7 @@ export default function Example() {
                     </a>
                   ))}
                   <a href="/" className=" border border-transparent text-base font-medium rounded text-white bg-rose-600 hover:bg-rose-700 md:py-3 md:text-lg md:px-4">
-                  {/* px-4 py-3 border border-transparent text-base font-medium rounded-md text-white bg-rose-600 hover:bg-rose-700 md:py-4 md:text-lg md:px-10 */}
+                  
                     Contato
                   </a>
                 </div>
@@ -114,8 +114,8 @@ export default function Example() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left ">
               <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
-                <span className="block xl:inline text-gray-900 font-extrabold">O Futuro</span>{' '}
-                <span className="block text-rose-600 xl:inline">Começa aqui</span>
+                <span className="block xl:inline text-gray-900 font-bold">O Futuro</span>{' '}
+                <span className="block text-rose-600 xl:inline ">Começa aqui </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -145,7 +145,7 @@ export default function Example() {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full "
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
