b6ddec0830c67b142360e07be5db266fb4af9388
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2022-09-01 09:53:00 -0300

    porfolio

--- site-here/src/components/Projects.jsx
+++ site-here/src/components/Projects.jsx
@@ -4,26 +4,66 @@
 
 import React from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 
 const Projects = () => {
   return (
     <div >
-        <Tabs>
+      <Tabs>
         <TabList className='flex items-center justify-center'>
-            <Tab>Title 1</Tab>
-            <Tab>Title 2</Tab>
-            <Tab>Title 3</Tab>
-            <Tab>Title 4</Tab>
+          <Tab className='px-5 py-1 mx-1 border-b-2 dark:border-gray-700 border-rose-400 text-rose-400'>Website Design</Tab>
+          <Tab className='px-5 py-1 mx-1 border-b-2 dark:border-gray-700'>Mobile</Tab>
+          <Tab className='px-5 py-1 mx-1 border-b-2 dark:border-gray-700'>E-Commerce</Tab>
+          <Tab className='px-5 py-1 mx-1 border-b-2 dark:border-gray-700'>Desktop</Tab>
         </TabList>
 
         <TabPanel>
-            <h2>Any content 1</h2>
+        <section className="py-6 ">
+            <div className="container flex flex-col justify-center p-4 mx-auto">
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?1" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?2" />
+                
+              </div>
+            </div>
+          </section>
         </TabPanel>
         <TabPanel>
-            <h2>Any content 2</h2>
+          <section className="py-6 ">
+            <div className="container flex flex-col justify-center p-4 mx-auto">
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?1" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?2" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?3" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?4" />
+              </div>
+            </div>
+          </section>
         </TabPanel>
-    </Tabs>
+        <TabPanel>
+        <section className="py-6 ">
+            <div className="container flex flex-col justify-center p-4 mx-auto">
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?3" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?4" />
+              </div>
+            </div>
+          </section>
+        </TabPanel>
+        <TabPanel>
+        <section className="py-6 ">
+            <div className="container flex flex-col justify-center p-4 mx-auto">
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?1" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?2" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?3" />
+                <img className="object-cover w-full  aspect-square" src="https://source.unsplash.com/random/300x300/?4" />
+              </div>
+            </div>
+          </section>
+        </TabPanel>
+      </Tabs>
     </div>
   )
 }
