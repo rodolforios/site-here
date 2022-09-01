d92b0281deb3f3cf6c267d5c17c9be7d1c61ca12
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2022-09-01 16:57:20 -0300

    portfólio com fotos

--- site-here/src/components/Clients.jsx
+++ site-here/src/components/Clients.jsx
@@ -9,30 +9,30 @@ const Clients = () => {
             <section className="p-6 bg-gray-50 text-gray-700">
 
                 <div className="container p-4 mx-auto text-center">
-                    <h2 className="text-4xl font-bold">Clientes que confiam na HereIT</h2>
+                    <h2 className="text-4xl font-bold">Clientes que <span className='border-b-4 border-rose-600'>Confiam</span> na HereIT</h2>
                 </div>
 
                 <div className="container flex flex-wrap justify-center mx-auto text-gray-400">
 
                     <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
-                        <img src="/logo scan do brasil.png" alt="" className='grayscale    h-14' />
+                        <img src="/logo scan do brasil.png" alt="" className='    h-14' />
 
                     </div>
 
                     <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
 
-                        <img src="/logo-vbank.png" alt="" className='grayscale    h-14' />
+                        <img src="/logo-vbank.png" alt="" className='    h-14' />
 
                     </div>
 
                     <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
 
-                        <img src="/logo midia.png" alt="" className='grayscale    h-14' />
+                        <img src="/logo midia.png" alt="" className='    h-14' />
 
                     </div>
 
                     <div className="flex justify-center w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4">
-                        <img src="/logo estudio didatico.png" alt="" className='grayscale    h-14' />
+                        <img src="/logo estudio didatico.png" alt="" className='    h-14' />
                     </div>
                     
                 </div>
--- site-here/src/components/Projects.jsx
+++ site-here/src/components/Projects.jsx
@@ -14,16 +14,16 @@ const Projects = () => {
   return (
 
     <div>
-      <h1 className=" px-4 mt-20 relative text-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">We invest in the world’s potential</h1>
-      <p className="mb-6 px-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+      <h1 className=" px-4 mt-20 relative text-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">Projetos já feitos pela HereIT</h1>
+      <p className="mb-6 px-4 text-lg relative text-center font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
 
       <Tabs>
         <section className="bg-white ">
           <div className="container px-6 py-12 mx-auto">
-            <h1 className="text-2xl font-semibold text-gray-800 lg:text-4xl ">Portfolio</h1>
+            <h1 className="text-2xl font-semibold text-gray-800 lg:text-4xl ">Portfólio</h1>
             <div className="mt-8 xl:mt-16 lg:flex lg:-mx-12">
               <div className="lg:mx-12">
-                <h1 className="text-xl font-semibold text-gray-800 ">Table of Content</h1>
+                <h1 className="text-xl font-semibold text-gray-800 ">Tipo de Produto</h1>
                 <TabList>
                   <div className="mt-4 space-y-4 lg:mt-8">
                     <Tab>
@@ -70,7 +70,7 @@ const Projects = () => {
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
                   <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup Scan do brasil.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Scan</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Website/Ecommerce</p>
@@ -78,35 +78,35 @@ const Projects = () => {
 
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/Mockup Vbank.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">V Bank</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                        src="/Mockup wise home.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Wise Home</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup midia na tv.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Midia na TV</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                        src="Mockup Loígico.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Estudio didático</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup Scan do brasil.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Scan</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Website/Ecommerce</p>
@@ -122,7 +122,7 @@ const Projects = () => {
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup Scan do brasil.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Scan</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Website/Ecommerce</p>
@@ -139,7 +139,7 @@ const Projects = () => {
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/Mockup Vbank.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">V Bank</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
@@ -153,14 +153,14 @@ const Projects = () => {
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup midia na tv.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Midia na TV</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
                     </div>
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                        src="Mockup Loígico.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Estudio didático</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Aplicativo</p>
@@ -180,7 +180,7 @@ const Projects = () => {
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
                     <div>
                       <img className="object-cover w-full rounded-lg h-96 "
-                        src="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                        src="/mockup Scan do brasil.png"
                         alt="" />
                       <h2 className="mt-4 text-2xl font-semibold text-gray-800 capitalize ">Scan</h2>
                       <p className="mt-2 text-lg tracking-wider text-rose-500 uppercase  ">Website/Ecommerce</p>
