cff4408bea82c6337c01b5cd239a56840b027fd0
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2022-09-01 16:08:21 -0300

    steps concluido

--- site-here/src/components/Projects.jsx
+++ site-here/src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
   return (
 
     <div>
-      <h1 className="flex px-4 mt-20 justify-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">We invest in the world’s potential</h1>
+      <h1 className=" px-4 mt-20 relative text-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">We invest in the world’s potential</h1>
       <p className="mb-6 px-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
 
       <Tabs>
